47d0b39fcb74174a2e16a69b4f6fae47bd9b3d48
CJW <cwal37@gmail.com>
2016-05-06 09:45:09 -0400

    Working on some nltk and cleanup
    removing capitalization and things like that in order to make the
    comparisons easier/uniform. Although maybe I didn't have to, I'll run a
    couple comparison tests.

--- fun_comparisons.py
+++ fun_comparisons.py
@@ -11,5 +11,22 @@ import numpy as np
 import time
 import os
 import pdb
+import nltk
 
 
+start = time.clock()
+
+# 36x faster to import the csv rather than an xlsx file
+df = pd.read_csv('total_scrape_1.csv')
+print df.columns
+
+print df.Answers.str.contains('Chicago').sum()
+
+answers = list(df['Answers'])
+lc_answers = [w.lower() for w in answers]
+df['lc_answers'] = lc_answers
+
+print df.lc_answers.str.contains('chicago').sum()
+
+end = time.clock()
+print (end-start)
